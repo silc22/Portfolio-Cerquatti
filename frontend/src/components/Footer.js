147e677cc6346c5cb8314275bf5aeeae5ecf8e87
silc22 <you@example.com>
2022-03-02 23:05:18 +0100

    styles

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,49 @@
 import React from "react";
 import { animateScroll as scroll } from "react-scroll"
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
 export default function Footer(){
 
  
     return(
-        <div className="footer__container" style={{marginLeft: "200px"}}>
-            <button onClick={()=>scroll.scrollToTop()} >
-                TO TOP
-            </button>
+        <div className="footer__container">
+            <div className="footer__icons">
+            <a
+                href="https://www.linkedin.com/in/silvana-cerquatti/"
+                target='_blank'
+                rel="noreferrer"
+                >
+                <LinkedInIcon/>
+                </a>
+                <a
+                href="https://wa.me/393457634584"
+                target='_blank'
+                rel="noreferrer"
+                >
+                <WhatsAppIcon/>
+                </a>
+                <a
+                href="https://github.com/silc22"
+                target='_blank'
+                rel="noreferrer"
+                >
+                <GitHubIcon/>
+                </a>
+            </div>
+            <div className="footer__text">
+                <p>
+                    Developed by Sil Cerquatti with ❤️
+                </p>
+                <p>
+                    Thanks to <i>"los pelados"</i> for support.
+                </p>
+            </div>
+            <div className="button__top">
+                <ArrowCircleUpIcon onClick={()=>scroll.scrollToTop()}/>
+            </div>
         </div>
     )
 
