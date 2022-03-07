b58e99f73f45eba90ff1a39a8e227c351792deee
silc22 <you@example.com>
2022-03-07 21:02:54 +0100

    redefiniendo botton de call to action

--- frontend/src/components/CallToAction.js
+++ frontend/src/components/CallToAction.js
@@ -1,6 +1,6 @@
 import React from "react";
-import button from "../assets/button.png"
-import buttonOn from "../assets/buttonOn.png"
+import button from "../assets/buttonOn.png"
+import buttonOn from "../assets/button.png"
 
 
 export default function CallToAction(props) {
@@ -13,9 +13,9 @@ export default function CallToAction(props) {
             <div className={isOff ? "button__container" : "button__container button__container--clicked"}>
             {
                 isOff ?  
-                <img src={button} alt="button"/>
-                : 
                 <img src={buttonOn} alt="button"/>
+                : 
+                <img src={button} alt="button"/>
             } 
             </div>
         </div>
