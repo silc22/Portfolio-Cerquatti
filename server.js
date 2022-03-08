46197780717b5f4b1386461945028a9a4c3bd1d0
silc22 <you@example.com>
2022-03-08 23:29:39 +0100

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const Router = require('./routes/routes');
 require('dotenv').config();
-require('./config/database');
+require('./config/dataBase');
 
 
 const app = express()
