99f4e78919d3f3c64096d1dd6fcdc7f8c721fbdc
silc22 <you@example.com>
2022-03-10 22:20:50 +0100

    heroku

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use('/api', Router)
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("frontend/build"));
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+      res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
     });
 }
 
